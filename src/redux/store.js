516b2041fb2cf24d1e880de7c29edc4e5641edb1
kholmatov izzatillo <izzatillokholmatov09@gmail.com>
2023-08-07 17:23:20 +0500

    buySell redux fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import tableSlice from './table/tableReducer'
-import responsiveSlice from "./responsive/responsiveSlice";
 
 export const store = configureStore({
   reducer: {
