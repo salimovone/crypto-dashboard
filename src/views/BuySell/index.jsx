0cee52afadb09b06560440f1167a96b8bfc0fbf3
kholmatov izzatillo <izzatillokholmatov09@gmail.com>
2023-08-08 15:52:40 +0500

    marketcap

--- src/views/BuySell/index.jsx
+++ src/views/BuySell/index.jsx
@@ -1,8 +1,8 @@
-import { AiOutlineDownload } from "react-icons/ai";
 import { TbArrowsExchange2 } from "react-icons/tb";
 import React, { useState } from "react";
 import './style.css'
 import Table from '../../components/BuySell-Table'
+import Tables from '../../components/Table-top'
 
 function BuySell() {
     const [bgbottom, setBgbottom] = useState('actives')
@@ -111,59 +111,10 @@ function BuySell() {
                         </div>
                     </div>
 
-                    <div>
-                        <div className="pt-10">
-                            <div>
-                                <p className="text-[24px] text-[#35446f]">Recent Buying Cryptocurrency</p>
-                                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
+                    <Tables />
 
-                        <div className="grid lg:grid-cols-3 md:grid-cols-1 xl:gap-0 lg:gap-5 gap-5 xl:grid-cols-5 mt-5">
-                            <div>
-                                <p className="text-[#35446f] font-medium">Latest actions <br /> (Showing 01 to 06 of 100)</p>
-                            </div>
-
-                            <div>
-                                <input type="date" className="h-[40px] w-[85%] lg:w-auto px-8 rounded-xl border border-blue-600" />
-                            </div>
-                            <div>
-                                <input type="date" className="h-[40px]  w-[85%] px-8 rounded-xl border border-blue-600" />
-                            </div>
-
-                            <div>
-                                <select id="countries" class="border px-8 w-[85%] h-[40px] bg-inherit border-blue-600 rounded-lg">
-                                    <option selected>Status</option>
-                                    <option value="US">United States</option>
-                                    <option value="CA">Canada</option>
-                                    <option value="FR">France</option>
-                                    <option value="DE">Germany</option>
-                                </select>
-                            </div>
-
-                            <div className="flex gap-2">
-                                <p className="pt-2">SortBy:</p>
-                                <div><input type="date" className="h-[40px] w-[80%] px-10 lg:px-6 rounded-xl border border-blue-600" /></div>
-                                <div className="rounded-full icons_buySell w-[40px] xl:-ml-8 text-[22px] text-blue-600 flex justify-center items-center h-[40px]  border border-blue-600">
-                                    <AiOutlineDownload />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="flex pt-10 gap-1">
-                            <p>Show</p>
-                            <div className="-mt-[2px]"><select id="countries" class="border p-1 bg-inherit border-blue-600 rounded-lg">
-                                <option selected>10</option>
-                                <option value="US">25</option>
-                                <option value="CA">50</option>
-                                <option value="FR">100</option>
-                            </select></div>
-                            <p>entired</p>
-                        </div>
-
-                        <div className="mt-5 w-[100%]">
-                            <Table/>
-                        </div>
+                    <div className="mt-5 w-[100%]">
+                        <Table />
                     </div>
                 </div>
             </div>
