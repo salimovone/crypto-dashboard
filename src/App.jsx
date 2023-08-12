042234530cf63db5ed24550258e421f1a18cf8f6
Mahammadov Bekzod <bekzodmahammadov627@gmail.com>
2023-08-12 17:02:21 +0500

    trading is finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import Dashboard from "./views/Home";
 import Marketcap from "./views/Marketcap";
 import Actives from "./views/Actives";
 import View from "./views/Transactions/View-Transaction";
+import Trading from "./views/Trading/";
 
 function App() {
   return (
     <div className="App ">
-      <div className="overflow-y-scroll h-screen overflow-x-hidden Scroll hidden lg:block">   
+      <div className="overflow-y-scroll h-screen overflow-x-hidden Scroll hidden lg:block">
         <SideBar />
       </div>
       <div className=" overflow-y-scroll h-screen">
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/buySell" element={<BuySell />} />
+          <Route path="/trading" element={<Trading />} />
           <Route path="/marketcap" element={<Marketcap />} />
-          <Route path="/buySell" element={<BuySell/>}/>
-          <Route path="/marketcap" element={<Marketcap/>}/>
-          <Route path="/activities" element={<Actives/>}/>
-          <Route path="/viewTransaction" element={<View/>}/>
+          <Route path="/activities" element={<Actives />} />
+          <Route path="/viewTransaction" element={<View />} />
         </Routes>
       </div>
     </div>
