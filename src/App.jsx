3ad3a1f111d1dffd8cdc8e194788fc54869b16ab
Mahammadov Bekzod <bekzodmahammadov627@gmail.com>
2023-08-12 18:25:15 +0500

    bugs is solved

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import BuySell from "./views/BuySell";
 import Dashboard from "./views/Home";
 import Marketcap from "./views/Marketcap";
 import Actives from "./views/Actives";
-import View from "./views/Transactions/View-Transaction";
+import View from "./views/Transactions/ViewTransaction";
 import Trading from "./views/Trading/";
 import Search from "./views/Transactions/SearchTransaction";
 import SingleTransaction from "./views/Transactions/SingleTransaction";
-
 
 function App() {
   return (
@@ -23,11 +22,11 @@ function App() {
           <Route path="/buySell" element={<BuySell />} />
           <Route path="/trading" element={<Trading />} />
           <Route path="/marketcap" element={<Marketcap />} />
-          <Route path="/buySell" element={<BuySell/>}/>
-          <Route path="/activities" element={<Actives/>}/>
-          <Route path="/viewTransaction" element={<View/>}/>
-          <Route path="/transactionSearch" element={<Search/>}/>
-          <Route path="/singleTransaction" element={<SingleTransaction/>} />
+          <Route path="/buySell" element={<BuySell />} />
+          <Route path="/activities" element={<Actives />} />
+          <Route path="/viewTransaction" element={<View />} />
+          <Route path="/transactionSearch" element={<Search />} />
+          <Route path="/singleTransaction" element={<SingleTransaction />} />
         </Routes>
       </div>
     </div>
