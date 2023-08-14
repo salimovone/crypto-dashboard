19c67569fe20c2cf7297c2675b6cb6d4406f6a56
Salimov Sardor <salimovone@gmail.com>
2023-08-14 01:48:53 -0700

    dashboard finished and apexCharts installed

--- src/components/HomeEarrings/index.jsx
+++ src/components/HomeEarrings/index.jsx
@@ -1,10 +1,142 @@
-import React from "react";
+import React, { useState } from "react";
 import { Left, Main, Right } from "./style";
 import BuySell from "../BuySellTable";
 import Button from "../Button";
-
+import Chart from "react-apexcharts";
 
 const HomeEarrings = () => {
+  const chartData = {
+          
+    series: [
+      {
+        name: 'Actual',
+        data: [
+          {
+            x: '2011',
+            y: 1292,
+            goals: [
+              {
+                name: 'Expected',
+                value: 1400,
+                strokeHeight: 5,
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2012',
+            y: 4432,
+            goals: [
+              {
+                name: 'Expected',
+                value: 5400,
+                strokeHeight: 5,
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2013',
+            y: 5423,
+            goals: [
+              {
+                name: 'Expected',
+                value: 5200,
+                strokeHeight: 5,
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2014',
+            y: 6653,
+            goals: [
+              {
+                name: 'Expected',
+                value: 6500,
+                strokeHeight: 5,
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2015',
+            y: 8133,
+            goals: [
+              {
+                name: 'Expected',
+                value: 6600,
+                strokeHeight: 13,
+                strokeWidth: 0,
+                strokeLineCap: 'round',
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2016',
+            y: 7132,
+            goals: [
+              {
+                name: 'Expected',
+                value: 7500,
+                strokeHeight: 5,
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2017',
+            y: 7332,
+            goals: [
+              {
+                name: 'Expected',
+                value: 8700,
+                strokeHeight: 5,
+                strokeColor: '#775DD0'
+              }
+            ]
+          },
+          {
+            x: '2018',
+            y: 6553,
+            goals: [
+              {
+                name: 'Expected',
+                value: 7300,
+                strokeHeight: 2,
+                strokeDashArray: 2,
+                strokeColor: '#775DD0'
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: 'bar'
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '60%'
+        }
+      },
+      colors: ['#00E396'],
+      dataLabels: {
+        enabled: false
+      },
+      legend: {
+        show: true,
+        showForSingleSeries: true,
+        customLegendItems: ['Actual', 'Expected'],
+        markers: {
+          fillColors: ['#00E396', '#775DD0']
+        }
+      }
+    },}
+
   return (
     <Main className="w-full">
       <Left className="w-600px overflow-x-scroll">
@@ -40,7 +172,12 @@ const HomeEarrings = () => {
           </div>
 
           <div>
-            
+            <Chart
+              options={chartData.options}
+              series={chartData.series}
+              type="bar"
+              height={'250'}
+            />
           </div>
         </div>
       </Right>
